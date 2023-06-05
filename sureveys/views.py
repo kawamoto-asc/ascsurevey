1d3d47fe23d422469008df76943fb66f6214a6ec
kawamoto <kawamoto@agesystem.com>
2023-06-05 17:32:49 +0900

    認証を実装したつもり

--- sureveys/views.py
+++ sureveys/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # トップ画面
+@login_required
 def top(request):
     return render(request, "sureveys/top.html")
