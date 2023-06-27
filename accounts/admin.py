e8809839753a6660c1307906e247ba6cca47f309
kawamoto <kawamoto@agesystem.com>
2023-06-27 16:36:22 +0900

    Information入力、メインヘッダ
    ・Information 入力画面カスタマイズ完了のつもり
    ・メインヘッダに、ログインユーザ名が表示されなくなってたので修正

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,7 @@ from sureveys.models import Information
 # 管理画面でのメンテクラス
 # Information画面
 class InformationAdmin(admin.ModelAdmin):
+    # 一覧設定
     list_display = ('info', 'created_by', 'created_format', 'update_by', 'updated_format')
     ordering = ['-created_at']      # -で降順 -なしは昇順
 
@@ -16,6 +17,22 @@ class InformationAdmin(admin.ModelAdmin):
 
     created_format.short_description = '作成日'
     updated_format.short_description = '更新日'
+    
+    # 入力設定
+    fields = ('info',)   # 入力するのは情報カラムだけ
+
+    # 保存の処理を上書き
+    def save_model(self, request, obj, form, change):
+        # 作成者が空なら新規
+        if not obj.created_by:
+            # 作成者へログインIDをセット
+            obj.created_by = request.user.username
+        else:
+            # 更新者へログインIDをセット
+            obj.update_by = request.user.username
+
+        # 保存
+        super().save_model(request, obj, form, change)
 
 # Register your models here.
 admin.site.register(Information, InformationAdmin)
