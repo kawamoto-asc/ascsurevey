dc60afa1817553028646c5b67cff0634a0ad7e16
kawamoto <kawamoto@agesystem.com>
2023-07-05 17:03:23 +0900

    ユーザマスタメンテナンス
    ・一覧画面の枠とURL等作成

--- sureveys/urls.py
+++ sureveys/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from . import views
 
 from sureveys.views import top
 
 urlpatterns = [
     path('', top, name='top'),
+    path('customusers/', views.CUsersListView.as_view(), name='list-cusers')
 ]
--- sureveys/views.py
+++ sureveys/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import datetime, timedelta
 from django.shortcuts import render
 from django.utils import timezone
-from sureveys.models import Information
+from django.views.generic import ListView
+from sureveys.models import Information, CustomUser
 
 # トップ画面
 @login_required
@@ -15,3 +17,8 @@ def top(request):
     context = {"informations": informations,
                "dsp_new_day": dsp_new_day}
     return render(request, "sureveys/top.html", context)
+
+# ユーザーマスタメンテナンス
+class CUsersListView(LoginRequiredMixin, ListView):
+    template_name = 'sureveys/customuser_list.html'
+    model = CustomUser
